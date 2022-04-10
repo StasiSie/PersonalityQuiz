43f3261b2de4641ef96de0ea2be25a66c408d5ba
StasiSie <mail.koshaksa@gmail.com>
2022-04-10 20:01:12 +0300

    questions added

---
+++ PersonalityQuiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Игорь Богданов on 10.04.2022.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
